0b3dd4b198b439915381964f7d3e4622050441fc
Bráian Pereira <braianlfp@gmail.com>
2023-01-30 21:18:24 -0300

    Ajueste sprint 3

--- Sprint 3/DA#3-10/Exercicios/sessao6.py
+++ Sprint 3/DA#3-10/Exercicios/sessao6.py
@@ -114,3 +114,60 @@ sub = calculo.subtracao(x, y)
 print(f"Somando:{x}+{y} = {som}")
 print(f"Subtraindo:{x}-{y} = {sub}")
 
+# 24
+
+class Ordenadora:
+    def __init__(self, lista):
+        self.listaBaguncada = lista
+
+    def ordenacaoCrescente(self):
+        self.listaBaguncada.sort()
+        return self.listaBaguncada
+
+    def ordenacaoDecrescente(self):
+        self.listaBaguncada.sort()
+        self.listaBaguncada.reverse()
+        return self.listaBaguncada
+
+
+crescente = Ordenadora([3, 4, 2, 1, 5])
+decrescente = Ordenadora([9, 7, 6, 8])
+
+print(crescente.ordenacaoCrescente())
+print(decrescente.ordenacaoDecrescente())
+
+
+# 25
+# Crie uma classe Avião que possua os atributos modelo, velocidade_maxima, cor e capacidade.
+# Defina o atributo cor de sua classe , de maneira que todas as instâncias de sua classe avião sejam da cor “azul”.
+# Após isso, a partir de entradas abaixo, instancie e armazene em uma lista 3 objetos da classe Avião.
+# Ao final, itere pela lista imprimindo cada um dos objetos no seguinte formato:
+# “O avião de modelo “x” possui uma velocidade máxima de “y”, capacidade para “z” passageiros e é da cor “w”.
+# Sendo x, y, z e w cada um dos atributos da classe “Avião”.
+
+# Valores de entrada:
+# modelo BOIENG456: velocidade máxima 1500 km/h: capacidade para 400 passageiros: Cor Azul
+# modelo Embraer Praetor 600: velocidade máxima 863km/h: capacidade para 14 passageiros: Cor Azul
+# modelo Antonov An-2: velocidade máxima de 258 Km/h: capacidade para 12 passageiros: Cor Azul
+
+class Aviao:
+    def __init__(self, modelo, velocidade_maxima, capacidade):
+        self.capacidade = capacidade
+        self.velocidade_maxima = float(velocidade_maxima)
+        self.modelo = modelo
+        self.cor = "Azul"
+
+    def print(self):
+        print(f"O avião de modelo {self.modelo} possui uma velocidade máxima de {self.velocidade_maxima}, "
+              f"capacidade para {self.capacidade} passageiros e é da cor {self.cor}")
+
+
+aviao = [
+    Aviao("BOIENG456", 1500, 400),
+    Aviao("Embraer Praetor 600", 863, 14),
+    Aviao("Antonov An-2", 258, 12)
+]
+
+for x in aviao:
+    x.print()
+
